cc7ff54c6750ceb8a318eaee0d67f12682b2ff5a
mkMohit1 <49286417+mkMohit1@users.noreply.github.com>
2023-09-25 09:19:39 +0530

    Add files via upload

---
+++ Placement cell/config/mongoose.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+const DB = ('mongodb://127.0.0.1:27017/placementCell');
+
+
+mongoose.connect(DB, {
+	useNewUrlParser: true,
+	useUnifiedTopology: true,
+});
+
+const db = mongoose.connection;
+
+db.on('error', console.error.bind(console, 'Error in connecting to MongoDB'));
+
+db.once('open', function () {
+	console.log('Connected to Database :: Mongodb');
+});
+
+module.exports = mongoose;
