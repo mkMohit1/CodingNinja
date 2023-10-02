d258c83ca6de6b857cf912adaceee6c448c4c52e
mkMohit1 <49286417+mkMohit1@users.noreply.github.com>
2023-10-02 20:46:34 +0530

    Delete CSV upload directory

--- CSV upload/routes/delete.js
+++
@@ -1,8 +0,0 @@
-const express=require('express');
-const router=express.Router();
-const deleteController=require('../controllers/delete_controller')
-
-// for routing a delete
-router.get('/:file',deleteController.delete);
-
-module.exports=router;
