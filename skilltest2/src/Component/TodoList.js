8fd3c82179cd1f96cb21147f29a598640026181a
mkMohit1 <49286417+mkMohit1@users.noreply.github.com>
2024-01-30 00:21:43 +0530

    Delete skilltest2 directory

--- skilltest2/src/Component/TodoList.js
+++
@@ -1,8 +0,0 @@
-import styles from "./TodoList.module.css";
-export default function TodoList(props) {
-    console.log(props.data)
-    return (
-        <div className={styles.listContainer}>
-        </div>
-    );
-}
