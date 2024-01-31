83a6132b03cced393de3f45177684e9b4bd92223
mkMohit1 <49286417+mkMohit1@users.noreply.github.com>
2024-02-01 01:07:26 +0530

    Add files via upload

---
+++ habittrackerskilltest/tailwind.config.js
@@ -0,0 +1,12 @@
+// tailwind.config.js
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+    "./public/index.html",
+    "./src/index.css", // Add your main CSS file here
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
